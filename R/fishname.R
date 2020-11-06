e51e1cda6f1ec3e77e238c0b44a3e62dc214bff3
Marine <marine.ballutaud@univ-lille.fr>
2020-11-06 11:58:02 +0100

    :green_heart: get function for species name

--- R/fishname.R
+++ R/fishname.R
@@ -1,3 +1,20 @@
-FISH <- function(){
-  readline(prompt="Choose a species by writing its Latin name (genus + species), enter your ANSWER: ")
+get_fish_name <- function(){
+  list_species <- readr::read_csv(here::here("data","list-species.csv"))
+
+
+  msg <- ""
+  for (i in 1:nrow(list_species)){
+   msg=c(msg, paste0(list_species[i, 1], ": ", list_species[i, 2]," ", list_species[i, 3],"\n")) #ponctuation uk
+  }
+
+
+  cat(msg)
+  response <- readline(prompt="Choose a species by selecting its ID number, enter your ANSWER: ")
+  response <- as.numeric(response)
+  if (response %in% list_species$ID){
+    fishlatinname <- paste(list_species[response, 2], list_species[response, 3])
+  } else {
+    stop("sorry, try again!") #genere une error donc repete le msg entre paraenthese
+  }
+  return(fishlatinname)
 }
