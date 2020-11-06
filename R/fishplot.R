058ca4b82abd8c5a224689d3a439ea0035fe158e
Fabienne Ribeyre <fabienne.ribeyre@cirad>
2020-11-06 10:35:25 +0100

    :earth_africa:

--- R/fishplot.R
+++ R/fishplot.R
@@ -46,7 +46,7 @@ fishplot <- function(data) {
     data <- dplyr::left_join(data, Echelle, by= "specie")
     cust_labeller <- function(x) paste0("Size group:", levels(Echelle$Class))
 
-  ggplot2::ggplot(data, aes(x = age,
+  plots <- ggplot2::ggplot(data, aes(x = age,
         y = length,
         color = specie )) +
     geom_line() +
@@ -54,4 +54,5 @@ fishplot <- function(data) {
        theme_bw() +
         xlab("Age of fish (years)")
        ylab("Estimated length (cm)")
+  print(plots)
 }
