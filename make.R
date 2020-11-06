40cee7176b393885694fd321adb194fe68431151
lclavareau <73823810+lclavareau@users.noreply.github.com>
2020-11-06 10:30:53 +0100

    :whale2: nettoyage make R

--- make.R
+++ make.R
@@ -12,23 +12,15 @@ devtools::load_all() #package installed and loaded
 
 
 #------------load data
-<<<<<<< HEAD
+
 #test <- "Please first choose a latin names or a vector containing latin names"
 #writeLines(text)
 espece <- c("Sphyrna lewini", "Thunnus albacares", "Mola mola")
 
 #------------run model
-test <- dataextract(espece)
-plotfish(test)
-=======
-espece <- "Salmo salar"
 data <- dataextract(espece)
+plotfish(data)
 
-#------------product plot
-fishplot(data)
-
-
->>>>>>> 1d18e94f00bae12b25390c9b5f1bde41c36c033d
 #-----------product article
 
 rmarkdown::render(here::here("outputs","publication","ElsevierRBertalanffyPackage.Rmd"))
