79c686821d73929cffe95120ade649b3a58b5b48
Marine <marine.ballutaud@univ-lille.fr>
2020-11-06 09:44:10 +0100

    :green_heart: make the make file

---
+++ make.R
@@ -0,0 +1,20 @@
+#!/usr/bin/env Rscript
+
+# ----- clean workspace
+rm(list = ls())
+
+# ----- install/update packages
+devtools::install_deps()
+
+# ----- install compendium package
+devtools::load_all() #package installed and loaded
+
+
+
+#------------load data
+
+
+#------------run model
+
+
+#-----------product article
